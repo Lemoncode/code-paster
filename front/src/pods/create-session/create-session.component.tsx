d230da81b00bfac5b265aea764ddf8366afcd71c
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-17 09:26:47 +0200

    create session btn added

--- front/src/pods/create-session/create-session.component.tsx
+++ front/src/pods/create-session/create-session.component.tsx
@@ -4,11 +4,17 @@ import { routes } from 'core/router';
 import { TestRunnerTrainerComponent } from './components/test-runner-trainer.component';
 import { TestRunnerStudentComponent } from './components/test-runner-student.component';
 
+// Material UI ~ components
+import Button from '@material-ui/core/Button';
+
 interface Props {}
 
 export const CreateSessionComponent: React.FunctionComponent<Props> = props => {
   return (
     <>
+      <Button variant="contained" color="primary">
+        Create Session
+      </Button>
       <h1>Create Session component</h1>
       <Link to={routes.trainer('myroom', 'mytrainertoken')}>
         Create Session - Navigate to trainer page
