094f9b002edc16c829dce0f61c078e07c275a17d
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-18 13:33:49 +0200

    lettuce removed

--- front/src/pods/create-session/create-session.component.tsx
+++ front/src/pods/create-session/create-session.component.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import * as classes from './create-session.styles';
-// import { Link } from 'react-router-dom';
-// import { routes } from 'core/router';
-// import { TestRunnerTrainerComponent } from './components/test-runner-trainer.component';
-// import { TestRunnerStudentComponent } from './components/test-runner-student.component';
-
 // Material UI ~ components
 import Button from '@material-ui/core/Button';
 
@@ -28,13 +23,6 @@ export const CreateSessionComponent: React.FunctionComponent<Props> = props => {
           Create Session
         </Button>
       </main>
-      {/* <h1>Create Session component</h1>
-      <Link to={routes.trainer('myroom', 'mytrainertoken')}>
-        Create Session - Navigate to trainer page
-      </Link>
-
-      <TestRunnerTrainerComponent />
-      <TestRunnerStudentComponent /> */}
     </>
   );
 };
