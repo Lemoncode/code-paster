144dee9216f865fdb4703369e31d7956d96e060f
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-10-06 13:26:59 +0200

    initial commit

--- front/src/core/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './api';
-export * from './const';
-export * from './use-log.hook';
--- front/src/scenes/create-session.scene.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CreateSessionContainer } from 'pods/create-session';
-
-export const CreateSessionScene: React.FC = () => {
-  return <CreateSessionContainer />;
-};
