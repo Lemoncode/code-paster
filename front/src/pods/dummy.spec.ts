a3cdb557700690a9f5c8fb4c13a1e39661462de9
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-10-19 11:25:26 +0200

    dummy test added

--- front/src/pods/dummy.spec.ts
+++ front/src/pods/dummy.spec.ts
@@ -1,1 +1,8 @@
 // TODO: remove this file when there is another spec file within the project
+
+describe('', () => {
+  it('', () => {
+    const value = true;
+    expect(value).toBeTruthy();
+  });
+});
