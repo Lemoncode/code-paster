25e9d381e8414e34b26c8a7fb15449e7c635c0cd
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-10-20 13:14:59 +0200

    adding theme

--- front/src/core/theme/theme.ts
+++ front/src/core/theme/theme.ts
@@ -3,19 +3,11 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#ff6666',
+      main: '#625261',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: '#D9D900',
-    },
-    success: {
-      main: '#43a047',
-    },
-    info: {
-      main: '#1976d2',
-    },
-    warning: {
-      main: '#ffa000',
+      main: '#89beb3',
     },
     text: {
       primary: '#2E2800',
@@ -23,8 +15,11 @@ export const theme = createMuiTheme({
     },
     divider: '#D9D900',
     background: {
-      paper: '#FCFAEA',
-      default: '#F2FEDC',
+      paper: '#ffffff',
+      default: '#ffffff',
     },
+  },
+  spacing: (n: number): string => {
+    return `${n / 16}rem`;
   },
 });
--- front/src/pods/trainer/components/new-text.styles.ts
+++ front/src/pods/trainer/components/new-text.styles.ts
@@ -1,4 +1,6 @@
 import { css } from 'emotion';
+import { theme } from 'core/theme';
+const { palette, typography, spacing } = theme;
 
 export const newTextContainer = css`
   width: 100%;
@@ -6,7 +8,7 @@ export const newTextContainer = css`
 `;
 
 export const labelTextarea = css`
-  font-family: 'Roboto', sans-serif;
+  font-family: ${typography.fontFamily};
 `;
 
 export const editTextArea = css`
@@ -14,9 +16,15 @@ export const editTextArea = css`
   width: 100%;
   margin-bottom: 10px;
   margin-top: 10px;
-  padding: 5px;
-  font-family: 'Roboto', sans-serif;
+  padding: ${spacing(10)};
+  font-family: ${typography.fontFamily};
+  background-color: ${palette.background.paper};
   resize: none;
+  border-radius: ${spacing(4)};
+  border: 1px solid ${palette.primary.main};
+  &:focus {
+    outline-color: ${palette.secondary.main};
+  }
 `;
 
 export const sendBtn = css`
