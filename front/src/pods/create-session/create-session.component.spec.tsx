016807a0fa12c6e905185a612b1b66be4d1eabba
Sergio Conde <serconde@gmail.com>
2020-11-16 21:58:48 +0100

    PR fixes

--- front/src/pods/create-session/create-session.component.spec.tsx
+++ front/src/pods/create-session/create-session.component.spec.tsx
@@ -4,6 +4,29 @@ import useEvent from '@testing-library/user-event';
 import { CreateSessionComponent } from './create-session.component';
 
 describe('CreateSessionComponent unit tests', () => {
+  it('should show the elements with the expected roles', () => {
+    // Arrange
+
+    const props = {
+      handleCreateSession: jest.fn(),
+    };
+
+    // Act
+
+    render(<CreateSessionComponent {...props} />);
+
+    const main = screen.getByRole('main');
+    const button = screen.getByRole('button');
+    const description = screen.getByText('The best tool for sharing code with your students!');
+
+    // Assert
+
+    expect(main).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Create Session');
+    expect(description).toBeInTheDocument();
+  });
+
   it('should create a new session when clicking the create session button', () => {
     // Arrange
 
@@ -21,5 +44,5 @@ describe('CreateSessionComponent unit tests', () => {
     // Assert
 
     expect(props.handleCreateSession).toHaveBeenCalledTimes(1);
-  })
+  });
 });
