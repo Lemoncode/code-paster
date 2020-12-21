b5c28ebb861dab999f634b540688904b1223067c
daniel.sanchez <daniel.sanchez@lemoncode.net>
2020-12-21 11:44:35 +0100

    remove unneeded fragment

--- front/src/pods/student/student.component.tsx
+++ front/src/pods/student/student.component.tsx
@@ -12,26 +12,24 @@ export const StudentComponent: React.FC<Props> = props => {
   const { room, log } = props;
 
   return (
-    <>
-      <main className={innerClasses.root}>
-        <Typography
-          className={innerClasses.sessionName}
-          variant="body1"
-          role="heading"
-        >
-          Session name: {room ?? ''}
-        </Typography>
-        <label className={innerClasses.label} htmlFor="session">
-          Content
-        </label>
-        <TextareaAutosize
-          id="session"
-          rowsMax={30}
-          rowsMin={30}
-          className={innerClasses.textarea}
-          value={log ?? ''}
-        />
-      </main>
-    </>
+    <main className={innerClasses.root}>
+      <Typography
+        className={innerClasses.sessionName}
+        variant="body1"
+        role="heading"
+      >
+        Session name: {room ?? ''}
+      </Typography>
+      <label className={innerClasses.label} htmlFor="session">
+        Content
+      </label>
+      <TextareaAutosize
+        id="session"
+        rowsMax={30}
+        rowsMin={30}
+        className={innerClasses.textarea}
+        value={log ?? ''}
+      />
+    </main>
   );
 };
