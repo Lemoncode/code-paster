1070b8775beb98aea79abedb81717ea056bc21bf
Fran Lopez <franciscolopto@gmail.com>
2023-04-18 19:28:56 +0200

    Updated components

--- front/src/common/markdown-view/markdown-view.tsx
+++ front/src/common/markdown-view/markdown-view.tsx
@@ -3,13 +3,13 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 interface Props {
-  value: string
+  value: string;
   className?: string;
 }
 
-export const MarkdownView: React.FC<Props> = (props) => {
+export const MarkdownViewer: React.FC<Props> = (props) => {
   return (
-    <div className={ props.className }>
+    <div className={props.className}>
       <ReactMarkdown children={props.value} remarkPlugins={[remarkGfm]} />
     </div>
   );
